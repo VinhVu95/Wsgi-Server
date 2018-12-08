b7fa631b0cec3860510b9533dcc4872b480b09da
Vinh Vu <anhvinhvu95@gmail.com>
2018-12-08 23:15:26 +0700

    Handle zombie process

--- concurent_server.py
+++ concurent_server.py
@@ -10,27 +10,40 @@
 import os
 import socket
 import time
+import signal
+import errno
 
 SERVER_ADDRESS = (HOST, PORT) = '', 8887
 REQUEST_QUEUE_SIZE = 5
 
 
-def handle_request(client_connection):
-    request = client_connection.recv(1024)
+def grim_reaper(signum, frame):
+    pid, status = os.wait()
     print(
-        'Child PID: {pid}. Parent PID {ppid}'.format(
-            pid=os.getpid(),
-            ppid=os.getppid(),
+        'Child {pid} terminated with status {status}'.format(
+            pid=pid, status=status
         )
     )
-    print(request.decode())
-    http_response = b"""\
-HTTP/1.1 200 OK
 
-Hello, World!
-"""
+
+def handle_request(client_connection):
+    request = client_connection.recv(1024)
+    print(request.decode())
+    #print(
+    #    'Child PID: {pid}. Parent PID {ppid}'.format(
+    #        pid=os.getpid(),
+    #        ppid=os.getppid(),
+    #    )
+    #)
+    #print(request.decode())
+    http_response = b"""\
+    HTTP/1.1 200 OK
+
+    Hello, World!
+    """
     client_connection.sendall(http_response)
-    #time.sleep(60)
+    # sleep to allow the parent to loop over to 'accept' and block there
+    time.sleep(3)
 
 
 def serve_forever():
@@ -41,9 +54,20 @@ def serve_forever():
     print('Serving HTTP on port {port} ...'.format(port=PORT))
     #print('Parent PID (PPID): {pid}\n'.format(pid=os.getpid()))
 
+    signal.signal(signal.SIGCHLD, grim_reaper)
+
     clients = []
     while True:
-        client_connection, client_address = listen_socket.accept()
+        try:
+            client_connection, client_address = listen_socket.accept()
+        except IOError as e:
+            code, msg = e.args
+            # restart 'accept' if it was interrupted
+            if code == errno.EINTR:
+                continue
+            else:
+                raise
+
         clients.append(client_connection)
         pid = os.fork()
         if pid == 0:  # child process
@@ -52,9 +76,9 @@ def serve_forever():
             client_connection.close()
             os._exit(0)  # child exits here
         else:  # parent process
-            # client_connection.close()  # close parent copy (file descriptor ref for client socket is now 1)
+            client_connection.close()  # close parent copy (file descriptor ref for client socket is now 1)
             # and loop over
-            print(len(clients))
+            # print(len(clients))
 
 if __name__ == '__main__':
     serve_forever()
